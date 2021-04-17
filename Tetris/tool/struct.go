966d866895b82af5b5aea8accf1706f937cb33e5
mspring03 <mspringmspring@naver.com>
2021-04-18 03:28:47 +0900

    🖕🏿

--- Tetris/tool/struct.go
+++ Tetris/tool/struct.go
@@ -1,5 +1,132 @@
 package tool
 
-type Shapes struct {
-
+func Shapes(shape int, num int) (arr [][]int) {
+	switch shape {
+	case 1:
+		switch num {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			arr := [][]int {
+				{0, 0}, {0, -1}, {-1, 0}, {-1, -1},
+			}
+			return arr
+		}
+	case 2:
+		switch num {
+		case 1:
+		case 3:
+			arr := [][]int {
+				{0, -2}, {0, -1}, {0, 0}, {0, 1},
+			}
+			return arr
+		case 2:
+		case 4:
+			arr := [][]int {
+				{1, 0}, {0, 0}, {-1, 0}, {-2, 0},
+			}
+			return arr
+		}
+	case 3:
+		switch num {
+		case 1:
+		case 3:
+			arr := [][]int {
+				{-1, -1}, {-1, 0}, {0, 0}, {0, 1},
+			}
+			return arr
+		case 2:
+		case 4:
+			arr := [][]int {
+				{1, 0}, {0, 0}, {0, 1}, {-1, 1},
+			}
+			return arr
+		}
+	case 4:
+		switch num {
+		case 1:
+		case 3:
+			arr := [][]int {
+				{0, -1}, {0, 0}, {-1, 0}, {-1, 1},
+			}
+			return arr
+		case 2:
+		case 4:
+			arr := [][]int {
+				{1, 1}, {0, 1}, {0, 0}, {-1, 0},
+			}
+			return arr
+		}
+	case 5:
+		switch num {
+		case 1:
+			arr := [][]int {
+				{-1, -1}, {0, -1}, {0, 0}, {0, 1},
+			}
+			return arr
+		case 2:
+			arr := [][]int {
+				{1, 0}, {0, 0}, {-1, 0}, {-1, 1},
+			}
+			return arr
+		case 3:
+			arr := [][]int {
+				{0, -1}, {0, 0}, {0, 1}, {1, 1},
+			}
+			return arr
+		case 4:
+			arr := [][]int {
+				{1, -1}, {1, 0}, {0, 0}, {-1, 0},
+			}
+			return arr
+		}
+	case 6:
+		switch num {
+		case 1:
+			arr := [][]int {
+				{0, -1}, {0, 0}, {0, 1}, {-1, 1},
+			}
+			return arr
+		case 2:
+			arr := [][]int {
+				{-1, 0}, {0, 0}, {1, 0}, {1, 1},
+			}
+			return arr
+		case 3:
+			arr := [][]int {
+				{1, -1}, {0, -1}, {0, 0}, {0, 1},
+			}
+			return arr
+		case 4:
+			arr := [][]int {
+				{-1, -1}, {-1, 0}, {0, 0}, {1, 0},
+			}
+			return arr
+		}
+	case 7:
+		switch num {
+		case 1:
+			arr := [][]int {
+				{0, -1}, {0, 0}, {0, 1}, {-1, 0},
+			}
+			return arr
+		case 2:
+			arr := [][]int {
+				{1, 0}, {0, 0}, {-1, 0}, {0, 1},
+			}
+			return arr
+		case 3:
+			arr := [][]int {
+				{0, -1}, {0, 0}, {0, 1}, {1, 0},
+			}
+			return arr
+		case 4:
+			arr := [][]int {
+				{0, -1}, {0, 0}, {1, 0}, {-1, 0},
+			}
+			return arr
+		}
+	}
+	return
 }
