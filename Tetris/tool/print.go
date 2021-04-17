3ecd3a82770b2cb63d62e9af489020a923603f61
mspring03 <mspringmspring@naver.com>
2021-04-18 03:30:34 +0900

    🖕🏿

--- Tetris/tool/print.go
+++ Tetris/tool/print.go
@@ -3,6 +3,7 @@ package tool
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"os/exec"
 )
 
 
@@ -11,254 +12,50 @@ import (
 //
 
 //fmt.Printf("This %s rocks!\n", info("package"))
-func Print(arr *[23][10]int) {
+func Print(arr [][]int) {
+	output, _ := exec.Command("clear").CombinedOutput()
+	fmt.Print(string(output))
 	println()
 	println()
 	border := color.New(color.BgHiWhite).SprintFunc()
-	for i := 0; i < 24; i++ {
+	for i := 22; i >= 0; i-- {
 		switch i {
-		case 0:
-			fmt.Printf("      %s                    %s", border("  "), border("  "))
-			fmt.Println()
-			continue
-		case 1:
-			fmt.Printf("      %s                    %s", border("  "), border("  "))
-			fmt.Println()
-			continue
-		case 2:
-			fmt.Printf("      %s﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘%s", border("  "), border("  "))
-			fmt.Println()
-			continue
-		case 3:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-					colorPrint(arr[i-3][j])
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 4:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 5:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 6:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 7:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 8:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 9:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 11:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 12:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 13:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 14:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 15:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 16:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 17:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 18:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 19:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
 		case 20:
 			fmt.Printf("      %s", border("  "))
 			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
+				if arr[i][j] == 0 {
+					print("﹘")
 				} else {
-
+					colorPrint(arr[i][j])
 				}
 			}
 			fmt.Printf("%s", border("  "))
 			fmt.Println()
 			continue
-		case 21:
+		case 0:
 			fmt.Printf("      %s", border("  "))
 			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
+				if arr[i][j] == 0 {
 					print("  ")
 				} else {
-
+					colorPrint(arr[i][j])
 				}
 			}
 			fmt.Printf("%s", border("  "))
 			fmt.Println()
-			continue
-		case 22:
-			fmt.Printf("      %s", border("  "))
-			for j := 0; j < 10; j++ {
-				if arr[i-3][j] == 0 {
-					print("  ")
-				} else {
-
-				}
-			}
-			fmt.Printf("%s", border("  "))
-			fmt.Println()
-			continue
-		case 23:
 			fmt.Printf("      %s%s%s%s%s%s%s%s%s%s%s%s", border("  "), border("  "), border("  "), border("  "), border("  "), border("  "), border("  "), border("  "), border("  "), border("  "), border("  "), border("  "))
+			fmt.Println()
+			continue
+		default:
+			fmt.Printf("      %s", border("  "))
+			for j := 0; j < 10; j++ {
+				if arr[i][j] == 0 {
+					print("  ")
+				} else {
+					colorPrint(arr[i][j])
+				}
+			}
+			fmt.Printf("%s", border("  "))
 			fmt.Println()
 			continue
 		}
@@ -266,14 +63,6 @@ func Print(arr *[23][10]int) {
 }
 
 func colorPrint(a int) {
-	color.New(color.BgHiCyan).SprintFunc()
-	color.New(color.BgRed).SprintFunc()
-	color.New(color.BgMagenta).SprintFunc()
-	color.New(color.BgHiBlue).SprintFunc()
-	color.New(color.BgBlue).SprintFunc()
-	color.New(color.BgGreen).SprintFunc()
-	color.New(color.BgYellow).SprintFunc()
-	//fmt.Printf("%s", v("  "))
 	switch a {
 	case 1:
 		Cyan := color.New(color.BgHiCyan).SprintFunc()
